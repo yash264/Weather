c1128e91cc42fcebcbc3e4e0daaec0f60647e49a
yash264 <141578029+yash264@users.noreply.github.com>
2023-10-03 18:13:50 -1200

    Update script.js

--- script.js
+++ script.js
@@ -18,11 +18,34 @@ const getWeather = (city) => {
 			humidity.innerHTML = response.humidity
 			max_temp.innerHTML = response.max_temp
 			min_temp.innerHTML = response.min_temp
-			sunrise.innerHTML = response.sunrise
-			sunset.innerHTML = response.sunset
 			temp.innerHTML = response.temp
 			wind_degrees.innerHTML = response.wind_degrees
 			wind_speed.innerHTML = response.wind_speed
+
+			const milliseconds1 = response.sunrise
+
+			var seconds1 = Math.floor(milliseconds1 / 1000);
+			var minutes1 = Math.floor(seconds1 / 60);
+			var hours1 = Math.floor(minutes1 / 60);
+
+			$seconds1 = seconds1 % 60;
+			$minutes1 = minutes1 % 60;
+			$hours1 = hours1 % 12;
+			
+			rise.innerHTML = $hours1+":"+$minutes1+":"+$seconds1;
+
+			const milliseconds2 = response.sunset
+
+			var seconds2 = Math.floor(milliseconds2 / 1000);
+			var minutes2 = Math.floor(seconds2 / 60);
+			var hours2 = Math.floor(minutes2 / 60);
+
+			$seconds2 = seconds2 % 60;
+			$minutes2 = minutes2 % 60;
+			$hours2 = hours2 % 12;
+			
+			set.innerHTML = $hours2+":"+$minutes2+":"+$seconds2;
+			
 		})
 		.catch(err => console.error(err))
 }
